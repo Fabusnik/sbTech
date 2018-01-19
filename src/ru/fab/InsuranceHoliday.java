f6a9c734b4807f459e9244faa6a03d1ba938f7dd
Fabus <aleksandrfab@gmail.com>
2018-01-19 10:41:12 +0700

    final

--- src/ru/fab/InsuranceHoliday.java
+++ src/ru/fab/InsuranceHoliday.java
@@ -1,21 +1,12 @@
 package ru.fab;
 
-import java.time.LocalDate;
-import java.util.Calendar;
 import java.util.Date;
 
-/**
- * Created by Aleks on 18.01.2018.
- */
 public class InsuranceHoliday {
     private Date holiday;
 
     public InsuranceHoliday(Date holiday) {
         this.holiday = holiday;
-    }
-
-    public Date getHoliday() {
-        return holiday;
     }
 
     public Date getTheDate(){
